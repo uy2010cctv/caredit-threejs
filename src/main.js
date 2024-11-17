df42beb089dd53cd83257cf33428d14946c793a0
78479 <784798006@qq.com>
2024-11-17 13:16:48 +0800

    微小修改

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { DecalGeometry } from 'three/addons/geometries/DecalGeometry.js';
-
-
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import { createLight } from "./js/light"
+import { ground } from "./js/ground"
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
 
 let renderer, camera;
 let line;
@@ -16,10 +15,10 @@ let stats;
 let car;
 let raycaster;
 let mouseHelper;
-let selectedIndex = -1;
+let selectedIndex;
 let index = 0;
 
-const dracoLoader = new DRACOLoader();
+//const dracoLoader = new DRACOLoader();
 const intersection = {
   intersects: false,
   point: new THREE.Vector3(),
@@ -46,12 +45,17 @@ let selectpart = {
   metalness: 0, 
 };
 
+
+
 const editmode = {
   decalsedit: false,
   coloredit: true,
 };
 
 const selectorContainer = document.getElementById('selector');
+const DecalsContainer = document.getElementById('decalslist');
+const listde = document.createDocumentFragment();
+const des = [];
 const list = document.createDocumentFragment();
 const imgs = [];
 
@@ -60,19 +64,13 @@ const decalsparams = {
 };
 
 const decalTextureUrls = [
-  'decals/doge.png',
-  'decals/dogs.png',
-  'decals/logo-tiger 1.png',
+  'decals/dogs.png'
   ];
 const textureLoader = new THREE.TextureLoader();
 let decalTextures = [];
 updateDecalTextures();
 
 const decalMaterial = new THREE.MeshBasicMaterial({
-  //specular: 0x444444,
-  map: decalTextures[1],
-  //normalScale: new THREE.Vector2( 1, 1 ),
-  //shininess: 10,
   transparent: true,
   depthTest: true,
   depthWrite: false,
@@ -105,13 +103,16 @@ let decalsIdCounter = 0;
 
 inti();
 
-function inti() {
+function inti() { 
+  createLight(scene);
+  ground(scene);
 
-  scene.background = new THREE.Color(0x333333);
-  scene.environment = new RGBELoader().load('venice_sunset_1k.hdr');
+  scene.environment = new RGBELoader().load('hdr1.hdr');
   scene.environment.mapping = THREE.EquirectangularReflectionMapping;
-  //scene.fog = new THREE.Fog(0x333333, 10, 30);
+  scene.environment.colorSpace = THREE.SRGBColorSpace;
   
+  scene.fog = new THREE.Fog(0x000000,3,20);
+  scene.background = new THREE.Color(0x000000);
 
   camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
   renderer = new THREE.WebGLRenderer({
@@ -125,7 +126,7 @@ function inti() {
   stats = new Stats();
   document.body.appendChild(stats.dom);
 
-  dracoLoader.setDecoderPath('jsm/libs/draco/gltf/');
+ //dracoLoader.setDecoderPath('jsm/libs/draco/gltf/');
   const geometry = new THREE.BufferGeometry();
 
 	geometry.setFromPoints( [ new THREE.Vector3(), new THREE.Vector3() ] );
@@ -135,47 +136,31 @@ function inti() {
   loadModel();
 
 
-  /*const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  // 设置光源的方向：通过光源position属性和目标指向对象的position属性计算
-  directionalLight.position.set(80, 100, 50);
-  // 方向光指向对象网格模型mesh，可以不设置，默认的位置是0,0,0
-  directionalLight.target.position.set(0, 0, 0);
-  scene.add(directionalLight);*/
-  // 辅助线
-  //const dirLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5,0xff0000);
-  //scene.add(dirLightHelper);
-
   //camera.position.z = 100;
-  camera.position.set(10, 10, 4);
+  camera.position.set(5,1,9);
   
   //camera.lookAt(0, 0, 0)
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enablePan = false; //禁止右键拖拽
 
-  const cx = 0,cy = 0,cz = 0; //通过OrbitControls辅助设置
+  const cx = 0,cy = 1.5,cz = 0; //通过OrbitControls辅助设置
   camera.lookAt(cx, cy, cz);
   // 相机控件.target属性在OrbitControls.js内部表示相机目标观察点，默认0,0,0
   // console.log('controls.target', controls.target);
   controls.target.set(cx, cy, cz); //与lookAt参数保持一致
   controls.update();
-  controls.minDistance = 7;
-  controls.maxDistance = 30;
+  controls.minDistance = 5;
+  controls.maxDistance = 10;
+  controls.minPolarAngle = 1.2;//最小相机倾角
+  controls.maxPolarAngle = 1.57;//最大相机倾角
   controls.rotateSpeed = 1; //旋转速度
   // 如果OrbitControls改变了相机参数，重新调用渲染器渲染三维场景
   controls.addEventListener('change', function () {
+
     renderer.render(scene, camera); //执行渲染操作
   }); //监听鼠标、键盘事件
 
-  /*距离消息
-  controls.addEventListener('change',function(){
-    //相机位置与目标观察点距离
-    const dis = controls.getDistance();
-    console.log('dis',dis);
-  })*/
-  /*var axesHelper = new THREE.AxesHelper(12); //坐标轴辅助线
-  scene.add(axesHelper);  */
 
- 
   mouseHelper = new THREE.Mesh(new THREE.TetrahedronGeometry(0.01, 0), new THREE.MeshNormalMaterial());
   //mouseHelper.visible = false;
   scene.add(mouseHelper);
@@ -184,12 +169,6 @@ function inti() {
   line = new THREE.Line(geometry, new THREE.LineBasicMaterial());
   scene.add(line);
 
-  //网格线绘制
-  var grid = new THREE.GridHelper(24, 24, 0xFF0000, 0x444444);
-  grid.material.opacity = 0.4;
-  grid.material.transparent = true;
-  //grid.rotation.x = Math.PI/2.0;
-  scene.add(grid);
 
 
 
@@ -198,11 +177,13 @@ function inti() {
 
   controls.addEventListener( 'change', function () {
     moved = true;
+   // console.log('controls.', controls)
+
   } );
 
   window.addEventListener( 'pointerdown', function () {
     moved = false;
-
+;
   } );
 
   window.addEventListener( 'pointerup', function ( event ) {
@@ -212,8 +193,7 @@ function inti() {
       checkIntersection( event.clientX, event.clientY-divObj.top );
       if(editmode.decalsedit === true){
       addDecal();
-      console.log('decals', decals);
-      //console.log('select', select.object);
+
     }
     updateGUI()
     try {
@@ -379,9 +359,11 @@ decalsFolder.close();
 decalsFolder.add(editmode, 'decalsedit').onChange(function(value) {
   if(editmode.decalsedit === true){
     document.getElementById("selector").style.display = 'flex';
+    document.getElementById("decalslist").style.display = 'flex';
     document.querySelector(".options-container").style.display = 'flex';
     }else{  
       document.getElementById("selector").style.display = 'none';  
+      document.getElementById("decalslist").style.display = 'none';
       document.querySelector(".options-container").style.display = 'none'; 
     }  
 });
@@ -394,7 +376,7 @@ decalsFolder.add(decalsparams,'Scale', 0, 1);
 
 function loadModel() {
   const loader = new GLTFLoader();
-  loader.setDRACOLoader(dracoLoader);
+ // loader.setDRACOLoader(dracoLoader);
   loader.load('ID3-app2.gltf'
   , function (gltf) {
     car = gltf.scene;
@@ -425,51 +407,18 @@ function createdecal(geometry, material) {
 
   const mesh = new THREE.Mesh(geometry, material);
 
-  // 设置网格的名字
+  // 设置decals的名字
   mesh.name = `decals_${decalsIdCounter}`;
 
   return mesh;
 }
 
-
-function setcameray() {    
-  camera.rotation.set(0, 1.5, 0);
-  camera.position.set(11, 0, 0.6);
-}
-
-function setcameray2() {    
-  camera.rotation.set(-3.14, -1.5, -3.14);
-  camera.position.set(-11, 0, -0.6);
-}
-
-function setcamerax() {    
-  camera.rotation.set(-0.27, 0.007, 0.002);
-  camera.position.set(0.09, 3.2, 11.5);
-}
-
-function setcamerax2() {    
-  camera.rotation.set(-2.92, -0.01, -3.13);
-  camera.position.set(-0.13, 2.6, -11.35);
-}
-let button = document.getElementById('rotateButtony');
-let button2 = document.getElementById('rotateButtony2');
-let button3 = document.getElementById('rotateButtonx');
-let button4 = document.getElementById('rotateButtonx2');
-// 添加点击事件监听器
-button.addEventListener('click', setcameray);
-button2.addEventListener('click', setcameray2);
-button3.addEventListener('click', setcamerax);
-button4.addEventListener('click', setcamerax2);
-
- 
 function addDecal() {
   //position.copy( intersection.point );
   position.z = intersection.point.z;
   position.y = intersection.point.y;
   position.x = intersection.point.x;
-  orientation.copy( camera.rotation );
-  console.log('camera.rotation', camera.rotation);
-  console.log('camera.position', camera.position);
+  orientation.copy( camera.rotation );  
   const material1 = decalMaterial.clone();
   if (selectedIndex >= 0) {
     index = selectedIndex;
@@ -486,13 +435,46 @@ try {
   m.renderOrder = decals.length; // give decals a fixed render order
 
   decals.push( m );
-
+  Decalslist();
   select.object.attach( m );
+  console.log('de', decals);
+  console.log('select', select.object.children);
+
 } catch (error) {
   //console.log('未选中目标');
 }
 }
 
+function Decalslist(){
+  des.length = 0;
+  decals.forEach((item, index) => {
+    const deDom = document.createElement('li');
+    deDom.classList.add('li');
+    deDom.id = `decal-${index}`;
+    deDom.textContent = item.name; 
+    deDom.addEventListener('click', () => {
+      des.forEach((item) => item.classList.remove('active'));
+      if (deDom.classList.contains('active')) {
+        deDom.classList.remove('active');
+      } else {
+        deDom.classList.add('active');
+      }
+    });
+    des.push(deDom);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+
+      Decalslist();
+    });
+    deDom.appendChild(deleteBtn);
+    listde.appendChild(deDom);
+  });
+  DecalsContainer.innerHTML = '';
+  DecalsContainer.append(listde);
+
+
+}
 
 function onWindowResize() {
 
@@ -567,6 +549,7 @@ document.getElementById('fileInput').addEventListener('change', function() {
 });
 //---------------------------------------------------------------------------------
 var render = function () {
+  camera.updateProjectionMatrix();
   requestAnimationFrame( render );
   stats.update();
   renderer.render(scene, camera);
